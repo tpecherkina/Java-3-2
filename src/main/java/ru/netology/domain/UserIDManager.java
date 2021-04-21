bfd436c9d283048f32dbea6389730c6f7a5445b2
tpecherkina <tpecherkina18@gmail.com>
2021-04-20 22:50:22 -0400

    Manager

---
+++ src/main/java/ru/netology/domain/UserIDManager.java
@@ -0,0 +1,12 @@
+package ru.netology.domain;
+
+import ru.netology.domain.UserID
+
+public class UserIDManager {
+    private UserID[] users;
+
+    public UserID[] getUsersIDs(String firstName, String lastName, String city, String country, String school, int age, boolean married,
+           boolean kids){
+        return null;
+    }
+}
